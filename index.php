c33f5f18374b338da5e6a72f8fd9023a6bf7ea7f
DMLmax <75531654+DMLmax@users.noreply.github.com>
2020-12-18 19:40:34 +0100

    ajout des fichier pour la carte de noël

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ma carte postale pour toi </title>
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<h1>Joyeux noël !! </h1>
+<p>Salut maman j'espere que tu va bien, je suis partir en vacance !</p>
+<p>j'aimerais que tu passe le bonjour a mamie et papie, que tu m'envoe mon cadeau car je suis toujours sage.</p>
+
+</body>
+</html>
